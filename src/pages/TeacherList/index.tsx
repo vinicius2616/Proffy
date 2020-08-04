2a9bf2b843a460bf9dfa7ad62b47b5a8949a862a
Vinicius Faleiros <42284565+vinicius2616@users.noreply.github.com>
2020-08-03 23:36:56 -0300

    First commit

---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
+
+export default function TeacherList() {
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os proffys disponíveis."/>
+    </div>
+  )
+}
